576781f4bc15dc7a2525ed662ac1e0eb0d33c8b0
nirbhay-design <sharma.59@iitj.ac.in>
2024-02-22 00:24:24 +0530

    hello

--- push_git.sh
+++ push_git.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+    echo "commit message is required"
+    exit 1
+fi
+
+commit_msg=$1
+
 git add .
-git commit -m "adding files"
+git commit -m "$commit_msg"
 git push origin master 
