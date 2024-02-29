e11cc95ce4e38e773bb26dd9fedfd193c3113634
nirbhay-design <sharma.59@iitj.ac.in>
2024-02-29 12:00:15 +0530

    CPVT implemented

--- CPVT.py
+++ CPVT.py
@@ -37,37 +37,27 @@ class PatchEmbedding(nn.Module):
 
         return patch_embeddings
     
-class ImageWPosEnc(nn.Module):
-    def __init__(self, image_size = 224, in_channels = 3, patch_size = 8, embed_size = 128):
+class PatchEmbeddingConv(nn.Module):
+    def __init__(self, image_size, in_channels = 3, patch_size = 8, embed_size = 128):
         super().__init__()
-        self.image_size = getHW(image_size) 
-        self.in_channels = in_channels
-        self.patch_size = getHW(patch_size) 
-        self.embed_size = embed_size 
-
-        self.patch_embed = PatchEmbedding(self.image_size,
-                                        self.in_channels,
-                                        self.patch_size,
-                                        self.embed_size)
-
-        self.num_embedding = (self.image_size[0] // self.patch_size[0]) * (self.image_size[1] // self.patch_size[1])
-        self.pos_encoding = nn.Parameter(torch.rand(1, self.num_embedding + 1, self.embed_size))
-        self.cls_token = nn.Parameter(torch.rand(1, 1, self.embed_size))
+        self.patch_size_h, self.patch_size_w = getHW(patch_size)
+        h,w = getHW(image_size)
+        assert h % self.patch_size_h == 0 and w % self.patch_size_w == 0, "image_size should be divisible by patch_size"
+        self.patch_conv = nn.Conv2d(in_channels, embed_size, kernel_size=3, padding = 1, stride = (self.patch_size_h, self.patch_size_w))
+        self.patch_size = patch_size
+        self.embed_dim = embed_size
 
     def forward(self, x):
-        B,_,_,_ = x.shape         
-        patch_embeddings = self.patch_embed(x)
-        pos_encoding = self.pos_encoding.repeat(B, 1, 1)
-        cls_token = self.cls_token.repeat(B, 1, 1)
-        patch_cls = torch.cat([cls_token, patch_embeddings], dim = 1)
-        embeddings = patch_cls + pos_encoding 
-
-        return embeddings
+        # x: [B, C, H, W]
+        B,C,H,W = x.shape
+        x = self.patch_conv(x) # [B, embed_dim, H/patch_size_h, W/patch_size_w]
+        x = x.permute(0, 2, 3, 1).reshape(B, -1, self.embed_dim)
+        return x
     
 class PEG(nn.Module):
     def __init__(self, image_size, kernel_size, embed_dim):
         super().__init__()
-        self.f = nn.Conv2d(embed_dim, embed_dim, kernel_size=kernel_size, padding = (kernel_size - 1) // 2)
+        self.f = nn.Conv2d(embed_dim, embed_dim, kernel_size=kernel_size, padding = (kernel_size - 1) // 2, groups=embed_dim)
         self.image_size = image_size
 
     def forward(self, x):
@@ -91,7 +81,7 @@ class Attention(nn.Module):
         return attention
 
 class MSA(nn.Module):
-    def __init__(self, num_heads = 8, embed_dim = 128):
+    def __init__(self, num_heads = 8, embed_dim = 128, dropout = 0.0):
         super().__init__()
         assert embed_dim % num_heads == 0, "num heads should be a multiple of embed_dim"
         self.head_dim = embed_dim // num_heads
@@ -99,7 +89,7 @@ class MSA(nn.Module):
         self.num_heads = num_heads
         self.embed_dim = embed_dim
         self.proj = nn.Linear(embed_dim, embed_dim)
-        self.proj_drop = nn.Dropout()
+        self.proj_drop = nn.Dropout(dropout)
 
     def forward(self, x):
         B, tokens, embed_dim = x.shape
@@ -127,7 +117,7 @@ class TransformerEncoder(nn.Module):
     def __init__(self, heads, embed_dim, mlp_hidden_dim = 256, dropout=0.0):
         super().__init__()
 
-        self.msa = MSA(heads, embed_dim)
+        self.msa = MSA(heads, embed_dim, dropout)
         self.mlp = MLP(embed_dim, mlp_hidden_dim, dropout=0.0)
         self.layer_norm = nn.LayerNorm(embed_dim)
         self.dropout = nn.Dropout(dropout)
@@ -152,38 +142,76 @@ class CPVT(nn.Module):
         
         super().__init__()
 
-        self.patch_creation = ImageWPosEnc(
-            image_size = image_size, 
-            in_channels = in_channels, 
-            patch_size = patch_size, 
-            embed_size = embed_dim
+        self.patch_creation = PatchEmbeddingConv(image_size,
+                                                 in_channels,
+                                                 patch_size,
+                                                 embed_dim)
+        
+        self.transformer_encoder1 = TransformerEncoder(msa_heads, embed_dim, hidden_dim, dropout)
+
+        img_h, img_w = getHW(image_size)
+        p_h, p_w = getHW(patch_size)
+        self.image_size = (img_h // p_h, img_w // p_w)
+        self.peg = PEG(image_size = self.image_size, kernel_size=3, embed_dim = embed_dim)
+
+        self.transformer_encoders = nn.Sequential(
+            *[TransformerEncoder(msa_heads, embed_dim, hidden_dim, dropout) for _ in range(encoder_layers - 1)]
         )
 
-        self.transformer_encoder = nn.Sequential(
-            *[TransformerEncoder(msa_heads, embed_dim, hidden_dim, dropout) for _ in range(encoder_layers)]
-        )
-
+        self.adaptive_avg_pooling = nn.AdaptiveAvgPool2d(output_size=(1,1))
         self.classification = nn.Linear(embed_dim, num_class)
 
     def forward(self, x):
         x = self.patch_creation(x)
-        x = self.transformer_encoder(x)
-        x = x[:,0,:]
-        x = x.flatten(1)
+        te1 = self.transformer_encoder1(x)
+        pe = self.peg(te1)
+        te1 = te1 + pe
+        x = self.transformer_encoders(te1)
+        B, _, embed_dim = x.shape
+        x = x.reshape(B, *self.image_size, embed_dim).permute(0, 3, 1, 2)
+        x = self.adaptive_avg_pooling(x).flatten(1)
         x = self.classification(x)
         return x
+    
+def cpvt_ti(image_size, num_class):
+    cpvt = CPVT(image_size=image_size,
+              in_channels=3,
+              patch_size=16,
+              encoder_layers=12,
+              msa_heads=3,
+              embed_dim=192,
+              hidden_dim=192,
+              num_class=num_class,
+              dropout=0.6)
+    return cpvt
+
+def cpvt_s(image_size, num_class):
+    cpvt = CPVT(image_size=image_size,
+              in_channels=3,
+              patch_size=16,
+              encoder_layers=12,
+              msa_heads=6,
+              embed_dim=384,
+              hidden_dim=384,
+              num_class=num_class,
+              dropout=0.6)
+    return cpvt
+
+def cpvt_b(image_size, num_class):
+    cpvt = CPVT(image_size=image_size,
+              in_channels=3,
+              patch_size=16,
+              encoder_layers=12,
+              msa_heads=12,
+              embed_dim=768,
+              hidden_dim=768,
+              num_class=num_class,
+              dropout=0.6)
+    return cpvt
 
 if __name__ == "__main__":
     a = torch.rand(2,3,224,224)
-    cpvt = CPVT(image_size=224,
-              in_channels=3,
-              patch_size=16,
-              encoder_layers=8,
-              msa_heads=8,
-              embed_dim=256,
-              hidden_dim=256,
-              num_class=10,
-              dropout=0.6)
+    cpvt = cpvt_b(224, 1000)
 
     out = cpvt(a)
     params = lambda x: sum([y.numel() for y in x.parameters()])
